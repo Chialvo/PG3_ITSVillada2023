7982cd45a89351146f7c73558018ed25b31a1a72
Chialvo <lautarochialvo2005@gmail.com>
2023-04-14 11:46:49 -0300

    Guia_3_ej4

---
+++ Ejercicios/Guia_3/ejercicio4.py
@@ -0,0 +1,14 @@
+while True:
+    try:
+        num1 = float(input("Ingrese el primer número: "))
+        num2 = float(input("Ingrese el segundo número: "))
+        resultado = num1 / num2
+        print("El resultado es:", resultado)
+    except ValueError:
+        print("Error: Ingreso un valor no numérico.")
+    except ZeroDivisionError:
+        print("Error: No se puede dividir por 0")
+    finally:
+        aux = input("¿Desea seguir sumando valores? (s/n): ")
+        if aux.lower() != 's':
+            break
